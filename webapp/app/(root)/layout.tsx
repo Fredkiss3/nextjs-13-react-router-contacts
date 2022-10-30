15e9bc55ee3dcd383b055d6e695ade9efa88939a
Adrien KISSIE <fredkiss3@gmail.com>
2022-10-30 14:42:16 +0100

    🚚 Refactor route folder in groups to have error handling at the top layout

---
+++ webapp/app/(root)/layout.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import { Sidebar } from "./sidebar";
+
+export type Contact = {
+  id: number;
+  createdAt: number; // timestamp
+  first?: string;
+  last?: string;
+  favorite?: boolean;
+  avatar?: string;
+  twitter?: string;
+  notes?: string;
+};
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const contacts = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER}/contacts`,
+    { cache: "no-store" }
+  ).then((r) => r.json() as Promise<Contact[]>);
+
+  return (
+    <main
+      style={{
+        height: `100vh`,
+        display: `flex`,
+      }}
+    >
+      <div id="sidebar">
+        <h1 style={{ display: "flex", gap: "1rem" }}>
+          <Image src="/nextjs.svg" alt="NextJS Logo" width={72} height={16} />{" "}
+          13 Contacts
+        </h1>
+        <Sidebar contacts={contacts} />
+      </div>
+      <div id="detail">{children}</div>
+    </main>
+  );
+}
