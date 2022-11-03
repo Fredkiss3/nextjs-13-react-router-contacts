32d315040ea6fa7d5913cba239617c7f11628b21
Adrien KISSIE <fredkiss3@gmail.com>
2022-11-03 23:15:00 +0100

    Added a suspense loading spinner

--- webapp/app/(root)/contacts/[id]/layout.tsx
+++ webapp/app/(root)/contacts/[id]/layout.tsx
@@ -7,24 +7,6 @@ export default function ContactLayout({
 }) {
   return (
     <>
-      <style>{`
-        .header {
-          background-color: #f7f7f7;
-          padding: 1rem 2rem;
-          width: 100%;
-          position: absolute;
-          inset-inline: 0;
-          top: 0;
-        }
-        .page {
-          margin-top: 4rem;
-        }
-        .nav-link {
-          text-decoration: none;
-          color: #3992ff;
-          font-weight: 500;
-        }
-      `}</style>
       <div className="header">
         <Link href={`/`} className="nav-link">
           {"<"} Go home
--- webapp/app/layout.tsx
+++ webapp/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Suspense } from 'react';
 
 export default async function RootLayout({
   children,
@@ -13,7 +14,35 @@ export default async function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </head>
-      <body>{children}</body>
+      <body>
+        <Suspense fallback={
+        <div style={{height: '100vh',width: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <svg
+            style={{height: `40px`,width: `40px`,}}
+            className={`animate-spin`}
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+        >
+            <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+            ></circle>
+            <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+        </svg>
+
+        </div>}>
+          {children}
+        </Suspense>
+      </body>
     </html>
   );
 }
